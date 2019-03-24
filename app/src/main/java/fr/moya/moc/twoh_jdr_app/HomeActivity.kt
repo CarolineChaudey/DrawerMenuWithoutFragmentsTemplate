209623da69729a61cb5e707e20fb7f4f9913d279
Caroline Chaudey <13118474+CarolineChaudey@users.noreply.github.com>
2019-03-24 20:44:50 +0100

    drawer menu navigation without fragments

---
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/HomeActivity.kt
@@ -0,0 +1,6 @@
+package fr.moya.moc.twoh_jdr_app
+
+import android.os.Bundle
+
+class HomeActivity : MainActivity(R.layout.activity_home) {
+}
---
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/MainActivity.kt
@@ -0,0 +1,80 @@
+package fr.moya.moc.twoh_jdr_app
+
+import android.os.Bundle
+import android.support.annotation.IdRes
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+open class MainActivity(protected val contentLayoutId: Int? = null) : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        if (contentLayoutId != null) {
+            layoutInflater.inflate(contentLayoutId, contentFrame as ViewGroup, true)
+        }
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_stats -> {
+
+            }
+            R.id.nav_inventory -> {
+
+            }
+            R.id.nav_spells -> {
+
+            }
+            R.id.nav_capacities -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
+
+/* fun <T : View> MainActivity.bind(@IdRes res : Int) : T {
+    @Suppress("UNCHECKED_CAST")
+    return findViewById(res)
+} */
