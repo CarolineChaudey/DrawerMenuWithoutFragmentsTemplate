b153e8244bb1632a5796bd22ac087612439aec43
Caroline Chaudey <13118474+CarolineChaudey@users.noreply.github.com>
2019-03-24 21:07:58 +0100

    home page is marked as selected in the menu

--- app/src/main/java/fr/moya/moc/twoh_jdr_app/HomeActivity.kt
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/HomeActivity.kt
@@ -1,4 +1,12 @@
 package fr.moya.moc.twoh_jdr_app
 
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 class HomeActivity : MainActivity(R.layout.activity_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        nav_view.menu.getItem(0).isChecked = true
+    }
 }
