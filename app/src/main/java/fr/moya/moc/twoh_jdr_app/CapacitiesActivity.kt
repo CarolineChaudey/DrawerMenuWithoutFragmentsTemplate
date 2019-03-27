d4e6c48aacfd951418fb1486f0395684d29e5449
Caroline Chaudey <13118474+CarolineChaudey@users.noreply.github.com>
2019-03-27 18:36:37 +0100

    add capacity

--- app/src/main/java/fr/moya/moc/twoh_jdr_app/CapacitiesActivity.kt
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/CapacitiesActivity.kt
@@ -1,6 +1,10 @@
 package fr.moya.moc.twoh_jdr_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import fr.moya.moc.twoh_jdr_app.models.Capacity
+import kotlinx.android.synthetic.main.activity_capacities.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class CapacitiesActivity : MainActivity(R.layout.activity_capacities) {
@@ -8,5 +12,10 @@ class CapacitiesActivity : MainActivity(R.layout.activity_capacities) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         nav_view.menu.getItem(3).isChecked = true
+        addCapacityButton.setOnClickListener { view ->
+            val intent = Intent(this, CapacityFormActivity::class.java)
+            val callback = { data: Capacity -> Log.e("CapacitiesActivity", data.toString()) }
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/CapacityFormActivity.kt
@@ -0,0 +1,36 @@
+package fr.moya.moc.twoh_jdr_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import fr.moya.moc.twoh_jdr_app.models.Capacity
+import io.paperdb.Paper
+import kotlinx.android.synthetic.main.activity_capacity_form.*
+
+class CapacityFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capacity_form)
+    }
+
+    fun saveCapacity(view: View) {
+        val capacity = Capacity(
+            capacityNameInput.text.toString(),
+            capacityRankInput.text.toInt(),
+            capacityBonusInput.text.toString(),
+            capacityHandicapInput.text.toString(),
+            capacityTotalInput.text.toString())
+        var capacities = Paper.book().read<MutableList<Capacity>>("capacities")
+        if (capacities == null) {
+            Log.e("CapacityFormActivity", "no capacities")
+            capacities = mutableListOf(capacity)
+        } else {
+            Log.e("CapacityFormActivity", capacities.toString())
+            capacities.add(capacity)
+        }
+        Paper.book().write("capacities", capacities)
+        finish()
+    }
+}
