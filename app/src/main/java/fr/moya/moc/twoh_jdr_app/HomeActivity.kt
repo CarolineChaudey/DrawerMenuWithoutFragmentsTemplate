5b540d51397d6a36259c1f55b41afd4db7e0c5ed
Caroline Chaudey <13118474+CarolineChaudey@users.noreply.github.com>
2019-03-27 15:10:26 +0100

    main info ready

--- app/src/main/java/fr/moya/moc/twoh_jdr_app/HomeActivity.kt
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/HomeActivity.kt
@@ -1,12 +1,68 @@
 package fr.moya.moc.twoh_jdr_app
 
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import fr.moya.moc.twoh_jdr_app.models.BasicInfo
+import io.paperdb.Paper
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class HomeActivity : MainActivity(R.layout.activity_home) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         nav_view.menu.getItem(0).isChecked = true
+        Paper.init(this)
+        val basicInfo = Paper.book().read<BasicInfo>("basic_info")
+        fillBasicInfo(basicInfo)
+    }
+
+    private fun fillBasicInfo(basicInfo: BasicInfo?) {
+        characterNameInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.name)
+        rpgClassInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.rpgClass)
+        raceInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.race)
+        lvlInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.lvl.toString())
+        xpInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.xp.toString())
+        hpInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.hp.toString())
+        maxHpInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.hpMax.toString())
+        destinyForceInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.forceDestinyPoints.toString())
+        destinyWisdomInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.wisdomDestinyPoints.toString())
+        destinyCourageInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.courageDestinyPoints.toString())
+        forceStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.forceStat.toString())
+        constitutionStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.constitutionStat.toString())
+        wisdomStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.wisdomStat.toString())
+        charismaStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.charismaStat.toString())
+        intelligenceStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.intelligenceStat.toString())
+        dexterityStatInput.text = Editable.Factory.getInstance().newEditable(basicInfo?.dexterityStat.toString())
+    }
+
+    fun saveData(view: View) {
+        val basicInfo = BasicInfo(characterNameInput.text.toString(),
+            rpgClassInput.text.toString(),
+            raceInput.text.toString(),
+            lvlInput.text.toInt(),
+            xpInput.text.toInt(),
+            hpInput.text.toInt(),
+            maxHpInput.text.toInt(),
+            destinyForceInput.text.toInt(),
+            destinyWisdomInput.text.toInt(),
+            destinyCourageInput.text.toInt(),
+            forceStatInput.text.toInt(),
+            constitutionStatInput.text.toInt(),
+            wisdomStatInput.text.toInt(),
+            charismaStatInput.text.toInt(),
+            intelligenceStatInput.text.toInt(),
+            dexterityStatInput.text.toInt())
+        Paper.book().write("basic_info", basicInfo)
+    }
+}
+
+fun Editable.toInt(): Int {
+    return try {
+        this.toString().toInt()
+    } catch (e: NumberFormatException) {
+        0
     }
 }
--- app/src/main/java/fr/moya/moc/twoh_jdr_app/models/BasicInfo.kt
+++ app/src/main/java/fr/moya/moc/twoh_jdr_app/models/BasicInfo.kt
@@ -17,23 +17,4 @@ data class BasicInfo(
                 val charismaStat: Int = 0,
                 val intelligenceStat: Int = 0,
                 val dexterityStat: Int = 0) {
-    companion object {
-        val TABLE_NAME = "basic_info"
-        val COL_NAME = "name"
-        val COL_CLASS = "rpgClass"
-        val COL_RACE = "race"
-        val COL_LVL = "level"
-        val COL_XP = "xp"
-        val COL_HP = "hp"
-        val COL_MAX_HP = "max_hp"
-        val COL_DEST_FORCE = "dest_force"
-        val COL_DEST_WISDOM = "dest_wisdom"
-        val COL_DEST_COURAGE = "dest_courage"
-        val COL_STAT_FORCE = "stat_force"
-        val COL_STAT_CONST = "stat_constitution"
-        val COL_STAT_WISDOM = "stat_wisdom"
-        val COL_STAT_CHARISMA = "stat_charisma"
-        val COL_STAT_INTELL = "stat_intelligence"
-        val COL_STAT_DEXT = "stat_dexterity"
-    }
 }
